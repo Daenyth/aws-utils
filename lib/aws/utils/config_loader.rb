e86e1f5ce115ce76d9ce5f75ace84cc92d375f96
Ed Ropple <ed@edropple.com>
2014-07-17 12:37:33 -0400

    Credentials loader works; checks for env vars, ~/.aws/config, and IAM (in that order)

---
+++ lib/aws/utils/config_loader.rb
@@ -0,0 +1,32 @@
+module Aws
+  module Utils
+    module ConfigLoader
+      public
+      def self.load()
+        filename = ENV["AWS_CONFIG_PATH"] || "#{ENV["HOME"]}/.aws/config"
+        if !File.exists?(filename)
+          return nil
+        end
+
+        ret = {}
+        current = "default"
+        File.readlines(filename).each do |line|
+          line = line.strip
+
+          if line == "[default]"
+            current = "default"
+          elsif line.start_with?("[profile")
+            current = /^\[profile ([A-Za-z\-\_\.0-9]+)\]$/.match(line)[1]
+          else
+            ret[current] ||= {}
+            tokens = /([A-Za-z0-9\-\_\.]+)\s*\=\s*(\S+)/.match(line)
+            if tokens
+              ret[current][tokens[1]] = tokens[2]
+            end
+          end
+        end
+        ret
+      end
+    end
+  end
+end
---
+++ lib/aws/utils/credentials.rb
@@ -0,0 +1,36 @@
+require "net/http"
+require "uri"
+
+require "aws/utils/config_loader"
+
+module Aws
+  module Utils
+    module Credentials
+      def self.acquire()
+        if (ENV["AWS_ACCESS_KEY_ID"] && ENV["AWS_SECRET_ACCESS_KEY"])
+          { 
+            :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+            :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
+          }
+        else
+          config_profile = ENV["AWS_PROFILE"] || "default"
+          config = Aws::Utils::ConfigLoader.load()
+          if config && config[config_profile]
+            {
+              :access_key_id => config[config_profile]["aws_access_key_id"],
+              :secret_access_key => config[config_profile]["aws_secret_access_key"]
+            }
+          else
+            iam_uri = URI.parse("http://169.254.169.254/latest/meta-data/iam/")
+            response = Net::HTTP.start(iam_uri.host, iam_uri.port) { |http| http.get(iam_uri.path) }
+            if response.code != 404
+              "IAM"
+            else
+              nil
+            end
+          end
+        end
+      end
+    end
+  end
+end
